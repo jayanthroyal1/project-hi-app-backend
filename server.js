9f10f293e5937667c1ebe4f49865e8c97a851d9f
Jayanth Royal <gjayanthroyal1@gmail.com>
2026-05-10 14:13:37 +0530

    added docker and ci basic config

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Backend working ..." });
+});
+
+app.listen(PORT, () => {
+  console.log("Backend working on port-", PORT);
+});
