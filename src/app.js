e7669c06752318df6fae1fa57ec8d470c9b4c7e4
Jayanth Royal <gjayanthroyal1@gmail.com>
2026-05-10 18:29:45 +0530

    adding some fixes

--- src/app.js
+++ src/app.js
@@ -4,7 +4,12 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.get("/health", (req, res) => {
