ff9c9562da0139b27a17f5bd6768855f3391a992
Jayanth Royal <gjayanthroyal1@gmail.com>
2026-05-11 14:17:54 +0530

    updating wit authentication-system mid-level

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+// Importing local files
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+connectDB();
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -11,6 +14,8 @@ app.use(
   }),
 );
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/health", (req, res) => {
   return res.status(200).json({
